8fcb00c8371fb09b8ef44ef6a7fa362b3690635b
mateovg <mvela092@fiu.edu>
2023-07-11 09:18:34 -0400

    fixed db generation and added user/pw data

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+# create the application object
 app = Flask(__name__)
+
+# home page
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# login page
+# from the realpython.com tutorial
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
+        # check if the username and password are correct
+        user = request.form['username']
+        password = request.form['password']
         return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
